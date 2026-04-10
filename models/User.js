199e45b4739ddf5f97d99d4e8086eef77b1ad6b8
Arpit Menaria <menariaarpit2@gmail.com>
2026-04-10 12:44:25 +0530

    push updated code

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     name: String,
-    email: { type: String, unique: true },
+    email: { type: String, unique: true, sparse: true },
     password: String,
     phone: { type: String, unique: true },
 
