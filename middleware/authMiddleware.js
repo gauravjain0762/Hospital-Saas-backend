0992d78460a7645dfb8c90878ac2e5d643f25492
Arpit Menaria <menariaarpit2@gmail.com>
2026-04-27 16:48:39 +0530

    updated

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -17,6 +17,10 @@ export const protect = async (req, res, next) => {
 
     req.user = await User.findById(decoded.id).select("-password");
 
+    if (!req.user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
